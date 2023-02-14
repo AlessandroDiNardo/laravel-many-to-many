2a99c411d20552953c5fa0fb6964d1a08aad9850
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-14 18:52:14 +0100

    create migrations

--- database/migrations/2023_02_14_173442_add_foreign_keys.php
+++ database/migrations/2023_02_14_173442_add_foreign_keys.php
@@ -8,17 +8,44 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+            $table -> foreignId('typology_id')
+                   -> constrained();
+        });
+
+        Schema::table('category_product', function (Blueprint $table) {
+            $table -> foreignId('category_id')
+                   -> constrained();
+
+            $table -> foreignId('product_id')
+                   -> constrained();
+        });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+           
+            $table -> dropForeign('products_typology_id_foreign');    
+            $table -> dropColumn('typology_id');      
+        });
+
+        Schema::table('category_product', function (Blueprint $table) {
+            $table -> dropForeign('category_product_category_id_foreign');
+            $table -> dropColumn('category_id');
+
+            $table -> dropForeign('category_product_product_id_foreign');
+            $table -> dropColumn('product_id');
+        });
     }
 };
