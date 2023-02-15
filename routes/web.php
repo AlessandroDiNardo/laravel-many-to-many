28f446be825efedfb65d00f00d93edf975f70e1c
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-15 15:52:59 +0100

    delete crud files, have to fix it

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 
-Route :: get('/', [MainController :: class , 'home'])
-    -> name('pages.home');
-
-// show
-Route :: get('/products', [MainController :: class , 'products'])
-    -> name('product.home');
-
-// create 
-Route :: get('/product/create',[MainController :: class , 'create'])
-    -> name('product.create');
-Route :: post('/product/create',[MainController :: class , 'store'])
-    ->name('product.store');
-
-// edit product
-Route :: get('/product/edit/{product}' , [MainController :: class, 'edit'])
-    -> name('product.edit');
-Route :: post('/product/edit/{product}' , [MainController :: class, 'update'])
-    -> name('product.update');
+Route::get('/', [MainController::class, 'home'])
+            ->name('home');
